945650d7d7daf3b31b6565691e08ace7b1c0b6dd
JenLeo <colleranjenny@gmail.com>
2021-05-04 11:40:02 +0100

    ##

--- GameAPI/GameClient.cs
+++ GameAPI/GameClient.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using System.Net.Http.Json;
-using Game.Controllers;
 
 namespace GameAPI
 {
